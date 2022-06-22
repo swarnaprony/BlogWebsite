13960a9175ebb72d103081b3e0deebfdaa0b7e17
Swarna Sarker <swarnaprony81@gmail.com>
2022-06-22 17:37:58 +0200

    Update post.ejs to add dynamic pages and home.ejs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const req = require('express/lib/request');
 const ejs = require("ejs");
+// Load the full build.
+var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -40,9 +42,19 @@ app.get("/compose", function(req, res) {
 
 
 app.get("/post/:title", (req, res) => {
-  if (titleItems.includes(req.params.title)) {
-    console.log(req.params.title);
-  };
+  const requstedTitle = req.params.title;
+
+  posts.forEach(function(post) {
+    const storedTitle = post.title;
+
+    if (_.lowerCase(storedTitle) === _.lowerCase(requstedTitle)) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+ 
 });
 
 
